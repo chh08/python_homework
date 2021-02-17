80ad3888f39b1195377e6f237081c9ad4d29177d
chh08 <1083260284@qq.com>
2021-02-17 17:16:54 +0800

    Delete money directory

--- money/money.py
+++
@@ -1,4 +0,0 @@
-# 定义变量save_money,存款1000元
-
-
-save_money = 1000
--- money/start.py
+++
@@ -1,8 +0,0 @@
-# 定义一个start.py ，启动文件展示最终存款金额
-from select_money import show_money
-from send_money import send_money
-
-if __name__ == '__main__':
-    send_money()
-    show_money()
-
